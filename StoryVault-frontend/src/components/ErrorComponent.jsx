191691e017a8e02f8bac24a894f4027ddf12db7f
Sundar Machani <sundarmachani@gmail.com>
2025-02-06 16:24:23 -0600

    version 1.2

---
+++ StoryVault-frontend/src/components/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+export default function ErrorComponent() {
+    return (
+      <div className="errorComponent">
+        <h1>Oops page not found ! 🥲</h1>
+      </div>
+    );
+  }
---
+++ StoryVault-frontend/src/components/LogoutComponent.jsx
@@ -0,0 +1,9 @@
+
+export default function LogoutComponent() {
+    return (
+      <div className="logoutComponent">
+        <h1>Logged out from the App !</h1>
+        <div>Thanks for sharing your daily stories see you soon 🤗</div>
+      </div>
+    );
+  }

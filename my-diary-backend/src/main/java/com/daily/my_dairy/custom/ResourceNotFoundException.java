32956c38be31453eac0186b563817ce7ca766061
Sundar Machani <sundarmachani@gmail.com>
2025-02-06 16:42:04 -0600

    v1.3

--- my-diary-backend/src/main/java/com/daily/my_dairy/custom/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.daily.my_dairy.custom;
-
-public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
-}
